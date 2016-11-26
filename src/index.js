a3a05dc27facfa310fe0b2f525d4940ce6890c15
Robin Dykema <robindykema@gmail.com>
2016-11-26 13:40:30 -0600

    29.5

--- src/index.js
+++ src/index.js
@@ -15,9 +15,16 @@ const API_KEY = 'AIzaSyCmIv1u3TcrSMHTh1TlP0BuQXhKdDRpmnI';
 class App extends Component {
 	constructor(props){
 		super(props);
-		this.state = { videos: [] };
+		this.state = { 
+			videos: [],
+			selectedVideo: null 
+		};
+
 		YTSearch({key: API_KEY, term: 'stupid cats'}, (videos) => {
-			this.setState({videos});
+			this.setState({
+				videos:videos,
+				selectedVideo: videos[0]
+			});
 //because the keyword matches,
 //this resolves as this.setState({videos:videos})
 		});
@@ -27,7 +34,7 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
-				<VideoDetail video={this.state.videos[0]}/>
+				<VideoDetail video={this.state.selectedVideo}/>
 				<VideoList videos={this.state.videos}/>
 			</div>
 		);
