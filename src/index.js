cc98fcb135aa1a1c8ea161b5bd0d776f2ed9853c
Robin Dykema <robindykema@gmail.com>
2016-11-26 02:18:11 -0600

    Use youtube api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCmIv1u3TcrSMHTh1TlP0BuQXhKdDRpmnI';
+
+
+
 // Create a new component.
 //This component should produce some html
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
+class App extends Component {
+	constructor(props){
+		super(props);
+		this.state = { videos: [] };
+		YTSearch({key: API_KEY, term: 'cats'}, (videos) => {
+			this.setState({videos});
+//because the keyword matches,
+//this resolves as this.setState({videos:videos})
+		});
+	}
+
+	render(){
+		return (
+			<div>
+				<SearchBar />
+			</div>
 		);
+	}
 }
 //Take this component's generated HTML and 
 //put it on the page (in the DOM)
